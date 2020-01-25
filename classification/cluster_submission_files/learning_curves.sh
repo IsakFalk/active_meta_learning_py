055349ed4f8d86896c98269667a394d85dfd286c
Isak Falk <me@isakfalk.com>
2020-01-25 16:32:37 +0000

    Add submission files for cluster

---
+++ classification/cluster_submission_files/learning_curves.sh
@@ -0,0 +1,98 @@
+#$ -S /bin/bash
+#$ -j y
+#$ -N aml_omniglot
+#$ -t 1-10
+
+#$ -l tmem=8G
+#$ -l h_vmem=8G
+#$ -l h_rt=8:0:0
+#$ -l gpu=true
+
+PROJECT_DIR=/cluster/project9/MMD_FW_active_meta_learning
+DATA_DIR=$PROJECT_DIR/data/
+SAVE_DIR=$PROJECT_DIR/experiments/learning_curves/${JOB_ID}_cnn_omniglot
+mkdir SAVE_DIR
+
+N_TRAIN_BATCHES=1000
+N_TEST_BATCHES=100
+TASKS_PER_METAUPDATE=16
+EVALUATE_EVERY=10
+
+N_WAY=5
+K_SHOT=1
+K_QUERY=15
+
+LR_INNER=0.5
+LR_META=0.001
+
+FRANK_WOLFE=kernel_herding
+KERNEL_FUNCTION=mean_linear
+
+NUM_GRAD_STEPS_INNER=1
+NUM_GRAD_STEPS_EVAL=1
+
+MODEL=cnn
+NUM_FILTERS=32
+HIDDEN_SIZE=64
+NUM_LAYERS=3
+
+DATASET=omniglot
+BASE_DATASET_TRAIN=train
+BASE_DATASET_VAL=val
+BASE_DATASET_TEST=test
+DATA_PATH=$DATA_DIR
+SAVE_PATH=$SAVE_DIR
+N_WORKERS=0
+
+# Remember to also export the path to the libraries and software needed, example
+export PATH=/share/apps/python-3.7.2-shared/bin:$PATH
+export LD_LIBRARY_PATH=/share/apps/python-3.7.2-shared/lib:$LD_LIBRARY_PATH
+
+# Log host and date
+hostname
+date
+
+# Only write config once
+if [ $SGE_TASK_ID -eq 1 ]
+then
+    WRITE_CONFIG=yes
+else
+    WRITE_CONFIG=no
+fi
+
+# Get into project dir
+cd /home/jitfalk/active_meta_learning_py/classification
+
+# Run it with seed specified by task id
+# NOTE: If you want to specify first order,
+# add --first_order flag to end.
+python3 main.py \
+        --n_train_batches $N_TRAIN_BATCHES \
+        --n_test_batches $N_TEST_BATCHES \
+        --seed $SGE_TASK_ID \
+        --tasks_per_metaupdate $TASKS_PER_METAUPDATE \
+        --evaluate_every $EVALUATE_EVERY \
+        --n_way $N_WAY \
+        --k_shot $K_SHOT \
+        --k_query $K_QUERY \
+        --lr_inner $LR_INNER \
+        --lr_meta $LR_META \
+        --frank_wolfe $FRANK_WOLFE \
+        --kernel_function $KERNEL_FUNCTION \
+        --num_grad_steps_inner $NUM_GRAD_STEPS_INNER \
+        --num_grad_steps_eval $NUM_GRAD_STEPS_EVAL \
+        --first_order $FIRST_ORDER \
+        --model $MODEL \
+        --num_filters $NUM_FILTERS \
+        --hidden_size $HIDDEN_SIZE \
+        --num_layers $NUM_LAYERS \
+        --dataset $DATASET \
+        --base_dataset_train $BASE_DATASET_TRAIN \
+        --base_dataset_val $BASE_DATASET_VAL \
+        --base_dataset_test $BASE_DATASET_TEST \
+        --data_path $DATA_PATH \
+        --save_path $SAVE_PATH \
+        --n_workers $N_WORKERS \
+        --write_config $WRITE_CONFIG \
+
+date
---
+++ classification/cluster_submission_files/submit_job.sh
@@ -0,0 +1,59 @@
+#$ -S /bin/bash
+#$ -j y
+#$ -N aml_omniglot
+
+#$ -l tmem=8G
+#$ -l h_rt=8:0:0
+#$ -l gpu=true
+#$ -wd /home/jitfalk/stdouterr/
+
+PROJECT_DIR=/cluster/project9/MMD_FW_active_meta_learning
+DATA_DIR=$PROJECT_DIR/data/
+SAVE_DIR=$PROJECT_DIR/experiments/learning_curves
+
+N_TRAIN_BATCHES=5000
+n_test_batches=200
+tasks_per_metaupdate=16
+evaluate_every=10
+
+n_way=5
+k_shot=1
+k_query=15
+
+lr_inner=0.5
+lr_meta=0.001
+
+frank_wolfe=kernel_herding
+kernel_function=mean_linear
+
+num_grad_steps_inner=1
+num_grad_steps_eval=1
+
+model=cnn
+num_filters=32
+hidden_size=54
+num_layers=64
+
+dataset=omniglot
+base_dataset_train=train
+base_dataset_val=val
+base_dataset_test=test
+data_path=$DATA_DIR
+save_path=$SAVE_DIR
+
+
+# Log host and date
+hostname
+date
+
+# Remember to also export the path to the libraries and software needed, example
+export PATH=/share/apps/python-3.7.2-shared/bin:/share/apps/julia-1.2.0/bin:$PATH
+export LD_LIBRARY_PATH=/share/apps/python-3.7.2-shared/lib:$LD_LIBRARY_PATH
+
+# Get into project dir
+cd /home/jitfalk/active_meta_learning_py/classification
+
+# Run main script
+python main.py --n_runs 5 --n_train_batches 1000 --n_test_batches 100 --tasks_per_metaupdate 16 \
+       --tasks_per_metaupdate 16 --evaluate_every 100 --n_way 5 --k_shot 1 --k_query 10 --num_epochs 5 \
+       --dataset omniglot --data_path $DATA_DIR --save_path
