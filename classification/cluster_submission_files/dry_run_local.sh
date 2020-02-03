226fdf8bc9c9364b7c26c8765495f0023d7b51ae
Isak Falk <me@isakfalk.com>
2020-02-03 14:04:03 +0000

    Delete old regression and clean up submission scripts

--- classification/cluster_submission_files/dry_run_local.sh
+++ classification/cluster_submission_files/dry_run_local.sh
@@ -1,52 +1,20 @@
-# Use this to test / dry run your jobs
-# Essentially running the code you would do
-# with `qsub` by running `qrsh -t` with
-# whatever -t options you have defined there
-# Remove any scheduler directive lines,
-# the ones starting with `#$`.
+#$ -S /bin/bash
+#$ -j y
+#$ -N aml_omniglot_maml
+# #$ -t 1-5
+#$ -wd /cluster/project9/MMD_FW_active_meta_learning/active_meta_learning_py/classification
 
-# Dummy variables, these will be set by the scheduler
-# when running `qsub`
+#$ -l tmem=2G
+#$ -l h_rt=10:0:0
+#$ -l gpu=true
 
 SGE_TASK_ID=1
 JOB_ID=TEST
 PROJECT_DIR=/home/isak/life/references/projects/phd/active_meta_learning/src/active_meta_learning_py/active_meta_learning/classification
 #PROJECT_DIR=/cluster/project9/MMD_FW_active_meta_learning
 DATA_DIR=$PROJECT_DIR/data/
-SAVE_DIR=$PROJECT_DIR/experiments/learning_curves/${JOB_ID}_mlp_sine_kh
+SAVE_DIR=$PROJECT_DIR/experiments/learning_curves/${JOB_ID}_cnn_omniglot_kh
 mkdir -p $SAVE_DIR
-
-
-N_TRAIN_BATCHES=5
-N_TEST_BATCHES=2
-TASKS_PER_METAUPDATE=2
-EVALUATE_EVERY=5
-
-N_WAY=5
-K_SHOT=1
-K_QUERY=15
-
-LR_INNER=0.5
-LR_META=0.4
-META_OPTIMIZER=sgd
-
-FRANK_WOLFE=kernel_herding
-KERNEL_FUNCTION=mean_linear
-
-NUM_GRAD_STEPS_INNER=1
-NUM_GRAD_STEPS_EVAL=1
-NUM_GRAD_STEPS_META=5
-
-NUM_FILTERS=64
-
-DATASET=omniglot
-BASE_DATASET_TRAIN=train
-BASE_DATASET_VAL=val
-BASE_DATASET_TEST=test
-DATA_PATH=$DATA_DIR
-
-SAVE_PATH=$SAVE_DIR
-N_WORKERS=0
 
 # Remember to also export the path to the libraries and software needed, example
 #source $PROJECT_DIR/project_environment.source
@@ -67,29 +35,29 @@ fi
 # NOTE: If you want to specify first order,
 # add --first_order flag to end.
 python3 main.py \
-        --n_train_batches $N_TRAIN_BATCHES \
-        --n_test_batches $N_TEST_BATCHES \
+        --n_train_batches 10 \
+        --n_test_batches 2 \
         --seed $SGE_TASK_ID \
-        --tasks_per_metaupdate $TASKS_PER_METAUPDATE \
-        --evaluate_every $EVALUATE_EVERY \
-        --k_shot $K_SHOT \
-        --k_query $K_QUERY \
-        --lr_inner $LR_INNER \
-        --lr_meta $LR_META \
-        --meta_optimizer $META_OPTIMIZER \
-        --frank_wolfe $FRANK_WOLFE \
-        --kernel_function $KERNEL_FUNCTION \
-        --num_grad_steps_inner $NUM_GRAD_STEPS_INNER \
-        --num_grad_steps_eval $NUM_GRAD_STEPS_EVAL \
-        --num_grad_steps_meta $NUM_GRAD_STEPS_META \
-        --num_filters $NUM_FILTERS \
-        --dataset $DATASET \
-        --base_dataset_train $BASE_DATASET_TRAIN \
-        --base_dataset_val $BASE_DATASET_VAL \
-        --base_dataset_test $BASE_DATASET_TEST \
-        --data_path $DATA_PATH \
-        --save_path $SAVE_PATH \
-        --n_workers $N_WORKERS \
+        --tasks_per_metaupdate 2 \
+        --evaluate_every 1 \
+        --k_shot 1 \
+        --k_query 5 \
+        --lr_inner 0.5 \
+        --lr_meta 0.4 \
+        --meta_optimizer sgd \
+        --frank_wolfe kernel_herding \
+        --kernel_function mean_linear \
+        --num_grad_steps_inner 1 \
+        --num_grad_steps_eval 1 \
+        --num_grad_steps_meta 1 \
+        --num_filters 64 \
+        --dataset omniglot \
+        --base_dataset_train train \
+        --base_dataset_val val \
+        --base_dataset_test test \
+        --data_path $DATA_DIR \
+        --save_path $SAVE_DIR \
+        --n_workers 0 \
         --write_config $WRITE_CONFIG
 
 date
