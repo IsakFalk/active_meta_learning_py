cbdd80b65ae70de472cf4a3001f751030a578647
Isak Falk <me@isakfalk.com>
2020-03-03 14:28:58 +0000

    Make project directory use __file__

--- active_meta_learning/project_parameters.py
+++ active_meta_learning/project_parameters.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
-parent_dir = Path("..")
-SCRIPTS_DIR = parent_dir / "scripts"
-JOB_SUBMISSION_DIR = parent_dir / "cluster_job_submissions"
-DATA_DIR = parent_dir / "data"
+PROJECT_DIR = Path(__file__).parent
+SCRIPTS_DIR = PROJECT_DIR / "scripts"
+JOB_SUBMISSION_DIR = PROJECT_DIR / "cluster_job_submissions"
+DATA_DIR = PROJECT_DIR / "data"
 INTERIM_DATA_DIR = DATA_DIR / "interim"
 JOB_OUTPUT_DIR = DATA_DIR / "job_output"
