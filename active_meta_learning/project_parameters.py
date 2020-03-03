b4e71362c5e4f042daac78a8c974010845448ea4
Isak Falk <me@isakfalk.com>
2020-03-03 00:48:51 +0000

    Upcase constants

--- active_meta_learning/project_parameters.py
+++ active_meta_learning/project_parameters.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
-parent_dir = Path('..')
-scripts_dir = parent_dir / 'scripts'
-job_submission_dir = parent_dir / 'cluster_job_submissions'
-data_dir = parent_dir / 'data'
-interim_data_dir = data_dir / 'interim'
-job_output_dir = data_dir / 'job_output'
+parent_dir = Path("..")
+SCRIPTS_DIR = parent_dir / "scripts"
+JOB_SUBMISSION_DIR = parent_dir / "cluster_job_submissions"
+DATA_DIR = parent_dir / "data"
+INTERIM_DATA_DIR = DATA_DIR / "interim"
+JOB_OUTPUT_DIR = DATA_DIR / "job_output"
