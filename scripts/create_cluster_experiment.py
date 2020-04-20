ff76eb05c95d93be44bd0bfb1f3e6baf8863dd64
Isak Falk <me@isakfalk.com>
2020-04-20 18:46:11 +0100

    Update cluster experiment

--- scripts/create_cluster_experiment.py
+++ scripts/create_cluster_experiment.py
@@ -4,18 +4,16 @@ from active_meta_learning import project_parameters
 from hpc_cluster.utils import ArrayJob
 
 param_dict = {
-    "d": [3, 5, 10],
-    "k": [2, 5, 8],
-    "noise_w": [0.01, 0.1, 1.0],
-    "num_prototypes": [5, 10]
+    "seed": list(range(10)),
+    "k_nn": [5],
+    "env_name": ["hypersphere-d=10.hkl"],
 }
 
 array_job = ArrayJob(
     param_dict=param_dict,
     working_dir=project_parameters.PROJECT_DIR,
     source_path=Path(project_parameters.PROJECT_DIR / "project.source"),
-    script_path=project_parameters.SCRIPTS_DIR
-    / "one_nn_conditional_active_meta_learning_gd.py",
+    script_path=project_parameters.SCRIPTS_DIR / "meta_knn" / "ridge",
     job_submission_files_dir=project_parameters.JOB_SUBMISSION_DIR,
     job_output_dir=project_parameters.JOB_OUTPUT_DIR,
     program="python3",
